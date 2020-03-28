295a7bd034418ee636573d5a414ed9c2511de981
Kseniia <kseniia@adminpdfs-MacBook-Pro-56.local>
2020-03-28 21:43:29 +0200

    add starosti page

--- page-starosti.php
+++ page-starosti.php
@@ -19,14 +19,61 @@ get_header(); ?>
 <div class  = "wrapper">
     <div class = "starosti">
         <h1>Старости академічних груп - кафедра Системного проектування</h1>
+    
+    <div class = "row">
+    <div class = "starosta">
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/stories/sda91.jpg" alt=""/>
+    <h3>Лавриненко Вероніка</h3>
+    <h4>Староста ДА-91</h4>
+    </div>
+    <div class = "starosta">
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/stories/sda92.jpg" alt=""/>
+    <h3>Кожемякін Володимир</h3>
+    <h4>Староста ДА-92</h4>
+    </div>
+    <div class = "starosta">
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/stories/sda93.jpg" alt=""/>
+    <h3>Косюк Олексій</h3>
+    <h4>Староста ДА-93</h4>
+    </div>
     </div>
     <div class = "row">
-    <div>
-    <p>djcfc</p>
+    <div class = "starosta"> 
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/stories/sda81.jpg" alt=""/>
+    <h3>Кангін Ілья</h3>
+    <h4>Староста ДА-81</h4>
     </div>
-    <div>
-    <p>djcfc</p>
+    <div class = "starosta">  
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/stories/sda82.jpg" alt=""/>
+    <h3>Прокопенко Катерина</h3>
+    <h4>Староста ДА-82</h4>
     </div>
+    </div>
+    <div class = "row">
+    <div class = "starosta"> 
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/stories/sda71.jpg" alt=""/>
+    <h3>Жук Валентина</h3>
+    <h4>Староста ДА-71</h4>
+    </div>
+    <div class = "starosta">  
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/stories/sda72.jpg" alt=""/>
+    <h3>Грищенко Олена</h3>
+    <h4>Староста ДА-72</h4>
+    </div>
+    </div>
+    <div class = "row">
+    <div class = "starosta"> 
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/stories/sda61.jpg" alt=""/>
+    <h3>Халета Марія</h3>
+    <h4>Староста ДА-61</h4>
+    </div>
+    <div class = "starosta">  
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/stories/sda62.jpg" alt=""/>
+    <h3>Сідельников Назар</h3>
+    <h4>Староста ДА-62</h4>
+    </div>
+    </div>
+
     </div>
 
 </div>
