ff1ea09daa565949a8bc58abc4367ce6819431dc
Kseniia <kseniia@adminpdfs-MacBook-Pro-56.local>
2020-04-01 12:29:41 +0300

    add rozklad page

---
+++ page-rozklad.php
@@ -0,0 +1,76 @@
+<?php /* Template Name: Rozklad */ 
+
+?>
+
+<?php
+/**
+ * @package start_theme
+ * 
+ */
+
+get_header(); ?>
+<html>
+<head>
+
+</head>
+<body>
+
+    
+<div class  = "wrapper">
+<div class = "bannerScience">
+        <div class = "column">
+        <div class = "text-banner">
+            <p><br></p>
+            <p><b>Не знаєш де опублікувати свою роботу?</b></p>
+            <p>Слідкуй за рубрикою <a href="/naukova-djalnst/konferencii" mce_href="/naukova-djalnst/konferencii">Конференції</a>.  Завжди корисна інформація, ключові дати та активні контакти!</p>
+            <p><br></p>
+        </div>
+        </div>
+    </div>
+    <div class = "tables">
+    <h2>Розклад  на 2019-2020 рік</h2>
+    <div style="overflow-x:auto;">
+  <table>
+    <tr>
+      <th>Файл</th>
+      <th>Розмір</th>
+      <th>Остання зміна</th>
+     </tr>
+    <tr>
+      <td><a class="at_icon" href="http://cad.kpi.ua/attachments/246_Розклад ДА-з91мп ( 5 курс ).pdf" target="_blank" title="Download this file"><img src="http://cad.kpi.ua/components/com_attachments/media/icons/pdf.gif" alt="Download this file"></a>
+      <a class="at_url" href="http://cad.kpi.ua/attachments/246_Розклад ДА-з91мп ( 5 курс ).pdf" target="_blank" title="Download this file">Розклад ДА-з91мп (5 курс)</a>
+    </td>
+      <td>115 Kb</td>
+      <td>16.09.2019 11:55</td>
+      </tr>
+    <tr>
+      <td><a class="at_icon" href="http://cad.kpi.ua/attachments/246_расп1-2019-20_1_2_3_4_5_6_dispet.pdf" target="_blank" title="Download this file"><img src="http://cad.kpi.ua/components/com_attachments/media/icons/pdf.gif" alt="Download this file"></a>
+      <a class="at_url" href="http://cad.kpi.ua/attachments/246_расп1-2019-20_1_2_3_4_5_6_dispet.pdf" target="_blank" title="Download this file ">Розклад 2019-2020 (1-6 курс)</a>
+    </td>
+      <td>515 Kb</td>
+      <td>16.09.2019 12:01</td>
+    </tr>
+    <tr>
+      <td><a class="at_icon" href="http://cad.kpi.ua/attachments/246_Teachers' loading schedule.pdf" target="_blank" title="Download this file"><img src="http://cad.kpi.ua/components/com_attachments/media/icons/pdf.gif" alt="Download this file"></a>
+      <a class="at_url" href="http://cad.kpi.ua/attachments/246_Teachers' loading schedule.pdf" target="_blank" title="Download this file">Графік завантаження викладачів факультету ІПСА кафедри СП на 1 семестр 2019/2020)</a>
+    </td>
+      <td>248 Kb</td>
+      <td>27.11.2019 18:38</td>
+      </tr>
+    <tr>
+      <td><a class="at_icon" href="http://cad.kpi.ua/attachments/246_audience_congestion_schedule.pdf" target="_blank" title="Download this file"><img src="http://cad.kpi.ua/components/com_attachments/media/icons/pdf.gif" alt="Download this file"></a>
+      <a class="at_url" href="http://cad.kpi.ua/attachments/246_audience_congestion_schedule.pdf" target="_blank" title="Download this file ">Графік завантаження аудиторій факультету ІПСА на 1 семестр 2019/2020 навчального</a>
+    </td>
+      <td>202 Kb</td>
+      <td>27.11.2019 18:39</td>
+    </tr>
+    </table>
+    </div>
+    </div>
+</div>
+
+
+
+<?php get_footer(); ?>
+</body>
+</html>
