2ed4e33d691fdae9628bbdcdcc2d71966efadf4d
Kseniia <kseniia@adminpdfs-MacBook-Pro-56.local>
2020-04-07 11:54:01 +0300

    fix pracevlashtuvannja js

--- js/expandable-block.js
+++ js/expandable-block.js
@@ -1,16 +1,4 @@
-function openContentProfession() {
-	var coll = document.getElementsByClassName("collapsible");
-	var i;
-	
-	for (i = 0; i < coll.length; i++) {
-	  coll[i].addEventListener("click", function() {
-		
-		var content = this.nextElementSibling;
-		if (content.style.display === "block") {
-		  content.style.display = "none";
-		} else {
-		  content.style.display = "block";
-		}
-	  });
-	}
-	}
+function expandableBlock(el) {
+    var content = el.nextElementSibling;
+    content.classList.toggle('open');
+   }
