3cf4b6471ab103348c8cb0720276db2c474a13c3
Kseniia <kseniia@adminpdfs-MacBook-Pro-56.local>
2020-04-08 19:03:05 +0300

    add phd page

--- functions.php
+++ functions.php
@@ -146,6 +146,7 @@ function start_theme_scripts() {
 	wp_enqueue_style( 'start-theme-personal', get_template_directory_uri().'/css/personal.css');
 	wp_enqueue_style( 'start-theme-pracevlashtuvannja', get_template_directory_uri().'/css/pracevlashtuvannja.css');
 	wp_enqueue_style( 'start-theme-chomu-sp', get_template_directory_uri().'/css/chomu-sp.css');
+	wp_enqueue_style( 'start-theme-phd', get_template_directory_uri().'/css/phd.css');
 	wp_enqueue_style( 'start-theme-footer', get_template_directory_uri().'/css/footer.css' );
 	wp_enqueue_style( 'start-theme-header', get_template_directory_uri().'/css/header.css' );
 	
---
+++ page-phd.php
@@ -0,0 +1,59 @@
+<?php /* Template Name: Phd */ 
+
+?>
+
+<?php
+/**
+ * @package start_theme
+ * 
+ */
+
+get_header(); ?>
+<html>
+<head>
+
+</head>
+<body>
+
+    
+<div class  = "wrapper">
+    <div class = "text-page">
+        <h1>Вступ на Аспірантуру&nbsp;</h1>
+        <p>Прийом документів до аспірантури відбудеться з 27 по 31 серпня 2018 року. <br>Умови прийому до аспірантури у 2018 році, детальна інформація про перелік необхідних документів, зразки їх оформлення, тощо розміщені на<a class = "allmode_link" href="http://aspirantura.kpi.ua/" mce_href="http://aspirantura.kpi.ua/"> сайті відділу аспірантури та докторантури</a>: www.aspirantura.kpi.ua</p>
+        <p>Звертаємо Вашу увагу, що до переліку документів, які подають вступники до аспірантури у 2018 році, внесений витяг з протоколу засідання кафедри (наукового семінару), де проходила презентація дослідницької пропозиції вступника.&nbsp;</p>
+        <p>Тому заслуховування презентацій дослідницьких пропозицій вступників до аспірантури у поточному році необхідно провести на засіданнях кафедр У ТРАВНІ- ЧЕРВНІ (НЕ ПІЗНІШЕ 08 ЧЕРВНЯ)</p>
+        <p>ДОКУМЕНТИ ДО ВСТУПУ ДО АСПІРАНТУРИ БУДУТЬ ПРИЙМАТИСЬ ВИКЛЮЧНО ПОВНИМ ОФОРМЛЕНИМ КОМПЛЕКТОМ.</p>
+        <div class = "tables">
+    
+    <div style="overflow-x:auto;">
+  <table>
+    <tr>
+      <th>Файл</th>
+      <th>Розмір</th>
+      <th>Остання зміна</th>
+     </tr>
+    <tr>
+      <td><a class="at_icon" href="http://cad.kpi.ua/attachments/471_471_ПОЛОЖ КМС (2).doc" target="_blank" title="Download this file"><img src="http://cad.kpi.ua/components/com_attachments/media/icons/word.gif" alt="Download this file "></a>
+      <a class="at_url" href="http://cad.kpi.ua/attachments/471_471_ПОЛОЖ КМС (2).doc" target="_blank" title="Download this file">ДОДАТКОВА ПРОГРАМА</a>
+    </td>
+      <td>89 Kb</td>
+      <td>31.03.2020 18:54</td>
+      </tr>
+    <tr>
+      <td><a class="at_icon" href="http://cad.kpi.ua/attachments/471_471_Програма_вступ_122_2018Комп_науки_СП (1).doc" target="_blank" title="Download this file"><img src="http://cad.kpi.ua/components/com_attachments/media/icons/word.gif" alt="Download this file "></a>
+      <a class="at_url" href="http://cad.kpi.ua/attachments/471_471_Програма_вступ_122_2018Комп_науки_СП (1).doc" target="_blank" title="Download this file">ПРОГРАМА ВСТУПНОГО ІСПИТУ PhD</a>
+    </td>
+      <td>90 Kb</td>
+      <td>31.03.2020 18:55</td>
+    </tr>
+    </table>
+    </div>
+    </div>
+
+</div>
+
+
+
+<?php get_footer(); ?>
+</body>
+</html>
